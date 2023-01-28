3fa2023ad57a839113fb221ebc26af920a167864
k.shimamura <34276109+KeisukeShimamura@users.noreply.github.com>
2023-01-28 23:24:50 +0900

    TOPページに各カテゴリの記事一覧を表示

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,14 +9,30 @@ import PostItemCassette from '../components/post-item-cassette'
 
 export const getStaticProps: GetStaticProps<{ posts: Post[] }> = () => {
   const { posts } = getPosts(undefined, 3, 1)
+  const { posts: freelancePosts } = getPosts('freelance', 5, 1)
+  const { posts: programingPosts } = getPosts('programing', 5, 1)
+  const { posts: hokkaidoPosts } = getPosts('hokkaido', 5, 1)
   return {
     props: {
       posts,
+      freelancePosts,
+      programingPosts,
+      hokkaidoPosts,
     },
   }
 }
 
-export default function Home({ posts }: { posts: Post[] }) {
+export default function Home({
+  posts,
+  freelancePosts,
+  programingPosts,
+  hokkaidoPosts,
+}: {
+  posts: Post[]
+  freelancePosts: Post[]
+  programingPosts: Post[]
+  hokkaidoPosts: Post[]
+}) {
   const [tabIndex, setTabIndex] = useState<number>(1)
 
   const clickTab = (index: number) => {
@@ -34,8 +50,7 @@ export default function Home({ posts }: { posts: Post[] }) {
       <div className="text-sm font-medium text-center text-gray-200 border-b border-gray-200">
         <ul className="flex flex-wrap -mb-px">
           <li className="w-1/3">
-            <a
-              href="#"
+            <button
               className={classNames(
                 'w-full inline-block p-4 border-b-2 rounded-t-lg',
                 tabIndex == 1
@@ -45,11 +60,10 @@ export default function Home({ posts }: { posts: Post[] }) {
               onClick={() => clickTab(1)}
             >
               フリーランスエンジニア
-            </a>
+            </button>
           </li>
           <li className="w-1/3">
-            <a
-              href="#"
+            <button
               className={classNames(
                 'w-full inline-block p-4 border-b-2 rounded-t-lg',
                 tabIndex == 2
@@ -59,11 +73,10 @@ export default function Home({ posts }: { posts: Post[] }) {
               onClick={() => clickTab(2)}
             >
               プログラミング
-            </a>
+            </button>
           </li>
           <li className="w-1/3">
-            <a
-              href="#"
+            <button
               className={classNames(
                 'w-full inline-block p-4 border-b-2 rounded-t-lg',
                 tabIndex == 3
@@ -73,25 +86,61 @@ export default function Home({ posts }: { posts: Post[] }) {
               onClick={() => clickTab(3)}
             >
               北海道フリーランス生活
-            </a>
+            </button>
           </li>
         </ul>
       </div>
-      <section>
-        {posts.map((post) => (
-          <div key={post.slug} className="my-6">
-            <PostItemCassette post={post} />
+      {tabIndex == 1 && (
+        <section>
+          {freelancePosts.map((post) => (
+            <div key={post.slug} className="my-6">
+              <PostItemCassette post={post} />
+            </div>
+          ))}
+          <div className="text-center mt-12">
+            <Link
+              href={`/category/freelance/page/1`}
+              className="inline-block border-t-2 border-b-2 px-24 py-2 border-gray-600 hover:bg-gray-600 hover:text-white"
+            >
+              もっと見る
+            </Link>
           </div>
-        ))}
-        <div className="text-center mt-12">
-          <Link
-            href={`/category/programing/page/1`}
-            className="inline-block border-t-2 border-b-2 px-24 py-2 border-gray-600 hover:bg-gray-600 hover:text-white"
-          >
-            もっと見る
-          </Link>
-        </div>
-      </section>
+        </section>
+      )}
+      {tabIndex == 2 && (
+        <section>
+          {programingPosts.map((post) => (
+            <div key={post.slug} className="my-6">
+              <PostItemCassette post={post} />
+            </div>
+          ))}
+          <div className="text-center mt-12">
+            <Link
+              href={`/category/programing/page/1`}
+              className="inline-block border-t-2 border-b-2 px-24 py-2 border-gray-600 hover:bg-gray-600 hover:text-white"
+            >
+              もっと見る
+            </Link>
+          </div>
+        </section>
+      )}
+      {tabIndex == 3 && (
+        <section>
+          {hokkaidoPosts.map((post) => (
+            <div key={post.slug} className="my-6">
+              <PostItemCassette post={post} />
+            </div>
+          ))}
+          <div className="text-center mt-12">
+            <Link
+              href={`/category/hokkaido/page/1`}
+              className="inline-block border-t-2 border-b-2 px-24 py-2 border-gray-600 hover:bg-gray-600 hover:text-white"
+            >
+              もっと見る
+            </Link>
+          </div>
+        </section>
+      )}
     </>
   )
 }
