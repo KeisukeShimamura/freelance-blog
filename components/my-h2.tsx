c7034d78052d212bb6700e6da3e10464da79835d
k.shimamura <34276109+KeisukeShimamura@users.noreply.github.com>
2023-02-05 00:21:14 +0900

    見出し要素にID付与

--- components/my-h2.tsx
+++ components/my-h2.tsx
@@ -1,7 +1,11 @@
 import React, { ReactNode } from 'react'
 
-const MyH2 = ({ children }: { children: ReactNode }) => {
-  return <h2 className="border-b border-emerald-600 pl-2 pb-1">{children}</h2>
+const MyH2 = ({ children, id }: { children: ReactNode; id: string }) => {
+  return (
+    <h2 id={id} className="border-b border-emerald-600 pl-2 pb-1">
+      {children}
+    </h2>
+  )
 }
 
 export default MyH2
--- components/my-h3.tsx
+++ components/my-h3.tsx
@@ -1,7 +1,11 @@
 import React, { ReactNode } from 'react'
 
-const MyH3 = ({ children }: { children: ReactNode }) => {
-  return <h3 className="border-l-4 border-emerald-600 pl-2 ml-2">{children}</h3>
+const MyH3 = ({ children, id }: { children: ReactNode; id: string }) => {
+  return (
+    <h3 id={id} className="border-l-4 border-emerald-600 pl-2 ml-2">
+      {children}
+    </h3>
+  )
 }
 
 export default MyH3
