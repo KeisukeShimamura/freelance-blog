ee9d1f07502da735c3b575f19e8018f63533998c
k.shimamura <34276109+KeisukeShimamura@users.noreply.github.com>
2023-02-23 22:44:37 +0900

    エラーになる記載を修正

--- components/footer.tsx
+++ components/footer.tsx
@@ -37,9 +37,9 @@ const Footer = () => {
             >
               <svg
                 fill="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 className="w-7 h-7"
                 viewBox="0 0 24 24"
               >
@@ -49,9 +49,9 @@ const Footer = () => {
             <a className="ml-3 text-gray-500">
               <svg
                 fill="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 className="w-5 h-5"
                 viewBox="0 0 24 24"
               >
